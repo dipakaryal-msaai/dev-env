b825938eea6efa86d697564deb99c462a48ca940
dipakaryal-msaai <11109360@uvu.edu>
2026-01-20 00:32:37 -0700

    removed calculator class to keep CalculatorApp class

--- calculator.py
+++ calculator.py
@@ -1,24 +1,5 @@
 # impoert math module for advanced mathematical operations
-import math
-
-
-# Simple calculator class
-class Calculator:
-    def add(self, a, b):
-        return a + b
-
-    def subtract(self, a, b):
-        return a - b
-
-    def multiply(self, a, b):
-        return a * b
-
-    def divide(self, a, b):
-        if b == 0:
-            raise ValueError("Cannot divide by zero.")
-        return a / b
-
-
+import math 
 # Calculator class with basic operations
 class CalculatorApp:
     def __init__(self, root):
